6bc5f22f402b8738b00bbb20b65d80fe6b4362ae
Dennis Schulmeister-Zimolong <dennis.schulmeister-zimolong@soapeople.com>
2022-07-04 16:02:32 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+Google Colab Pip Helper
+=======================
+
+This is just a small utility to help installing python libraries from a private
+git repository (on GitHub) in Google Colab. This requires a "Personal Access Token"
+to be generated in GitHub and used as part of the package url, like this:
+
+`!pip install git+https://Token@github.com/User/Repo`
+
+The problem with this approach is, that the token is exposed in the Colab notebook.
+This library thus allows to put the access token in a separate text file and use
+that for the installation.
+
+  1. Create a new personal access token in GitHub with "repo" authorization
+  2. Save the access token to a text file in your Google Drive
+  3. Install the private library in your notebook like this:
+
+     ```
+     import gcolab_pip_helper as gcph
+     github_token = gcph.read_token(("Colab Notebooks", "Tests", "github_token.txt"))
+     gcph.pip_install("git+https://%s@github.com/User/Repo" % github_token)
+     ```
+
+This is basicaly a simpified version of [colab-utils](https://github.com/namiyousef/colab-utils).
+
+Copyright
+---------
+
+This code is relased under the public domain.
---
+++ src/gcolab_pip_helper/__init__.py
@@ -0,0 +1,42 @@
+import os, subprocess, sys
+
+def read_token(path_parts):
+    """
+    Read GitHub access token from a text file on Google Drive.
+
+    Parameters:
+      path_parts: Sequence of filename parts, e.g. ("Colab Notebooks", "Tests", "github_token.txt")
+  
+    Returns:
+      Token read from the text file
+    """
+    path = os.path.join("", "drive", "My Drive")
+    token = ""
+
+    for part in path_parts:
+      path = os.path.join(path, part)
+  
+    with open(path, "r", encoding="utf-8") as token_file:
+      token = token_file.read().strip()
+  
+    return token
+
+def pip_install(pkg_name):
+    """
+    Install a package with pip, akin to `!pip install ...` but use
+    the Python kernel. This allos to replace a placeholder in the
+    package URL with the GitHub access token like so:
+
+    >>> import gcolab_pip_helper as gcph
+    >>> github_token = gcph.read_token(("Colab Notebooks", "Tests", "github_token.txt"))
+    >>> gcph.pip_install("git+https://%s@github.com/User/Repo" % github_token)
+    """
+    output = subprocess.run([sys.executable, "-m", "pip", "install", pkg_name], capture_output=True)
+  
+    if output.returncode == 0:
+      print("Success!")
+    else:
+      print("Error! Return code %s" % output.returncode)
+      print()
+      print("Standard Output:\n  %s\n\n" % output.stdout)
+      print("Standard Error:\n  %s\n" % output.stderr)
